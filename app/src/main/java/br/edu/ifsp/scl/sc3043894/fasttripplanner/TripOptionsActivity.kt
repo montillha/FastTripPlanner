6171d483e24323a9102ac1c49267d7740c40eabe
montillha <marianaalmeida235b@gmail.com>
2026-04-27 23:11:46 -0300

    feat: add accommodation type selection with radio buttons in TripOptionsActivity

--- app/src/main/java/br/edu/ifsp/scl/sc3043894/fasttripplanner/TripOptionsActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3043894/fasttripplanner/TripOptionsActivity.kt
@@ -4,16 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.edu.ifsp.scl.sc3043894.fasttripplanner.ui.theme.FastTripPlannerTheme
 
 class TripOptionsActivity : ComponentActivity() {
@@ -44,11 +55,41 @@ class TripOptionsActivity : ComponentActivity() {
 @Composable
 fun TripOptions(modifier: Modifier = Modifier, destiny: String,duration:String, dailyBudget: String){
 
+    val tripOptions = listOf("Econômica","Conforto","Luxo");
+    var optionSelected by rememberSaveable() { mutableStateOf("") }
     Column(
-        modifier = modifier.fillMaxWidth().padding(5.dp)) {
+        modifier = modifier.fillMaxWidth().padding(5.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
         Text(
-            text = "${destiny}, ${duration}, $dailyBudget"
+                text = "Escolha de Hospedagem",
+                modifier= Modifier.fillMaxWidth().padding(10.dp),
+                fontSize = 24.sp,
         )
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            // SpaceEvenly: distribui os filhos em espaço igual entre eles, nas bordas e entre cada item
+            horizontalArrangement = Arrangement.SpaceEvenly
+
+        ) {
+            tripOptions.forEach { tripOption ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RadioButton(
+                        selected = optionSelected == tripOption,
+                        onClick = { optionSelected = tripOption }
+                    )
+                    Text(
+                        text = tripOption
+                    )
+                }
+            }
+        }
+
+
+
+
     }
 
 
